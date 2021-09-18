ab93fa0ac66194138444a176b242f6a59d53c323
Chris Waterbury <chris@winnonapartners.com>
2021-09-18 12:55:42 -0400

    i need a litner or something that warns me this wont compile

--- src/server/scripts/Northrend/Naxxramas/boss_noth.cpp
+++ src/server/scripts/Northrend/Naxxramas/boss_noth.cpp
@@ -116,7 +116,6 @@ public:
             for (uint8 i = 0; i < count; ++i)
             {
                 Creature* cr = me->SummonCreature(entry, summoningPosition[urand(0, 4)]);
-                cr->
                 cr->SetMaxHealth(cr->GetMaxHealth()*RAID_MODE(1,5));
                 cr->SetHealth(cr->GetMaxHealth());
             }
