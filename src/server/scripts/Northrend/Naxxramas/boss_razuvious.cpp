ec23a31395cb902d18e16d2df88b7e28a7af98ec
Chris Waterbury <chris@winnonapartners.com>
2021-09-18 15:05:01 -0400

    make respawn with health when despawned

--- src/server/scripts/Northrend/Naxxramas/boss_razuvious.cpp
+++ src/server/scripts/Northrend/Naxxramas/boss_razuvious.cpp
@@ -84,9 +84,9 @@ public:
         void SummonedCreatureDespawn(Creature* cr) override
         {
             if (me->IsInCombat()) {
-              Creature* cr = me->SummonCreature(NPC_DEATH_KNIGHT_UNDERSTUDY, 2762.23f, -3085.07f, 267.685f, 1.95f);
-              cr->SetMaxHealth(cr->GetMaxHealth()*RAID_MODE(2,5));
-              cr->SetHealth(cr->GetMaxHealth());
+              Creature* newCr = me->SummonCreature(NPC_DEATH_KNIGHT_UNDERSTUDY, 2762.23f, -3085.07f, 267.685f, 1.95f);
+              newCr->SetMaxHealth(cr->GetMaxHealth()*RAID_MODE(2,5));
+              newCr->SetHealth(cr->GetHealth());
               summons.Summon(cr);
               summons.DoZoneInCombat();
             }
