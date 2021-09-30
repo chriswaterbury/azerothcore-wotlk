3daf21d52904df82e70f46b23337659d7a270409
Chris Waterbury <chris@winnonapartners.com>
2021-09-29 23:12:27 -0400

    mimi

--- src/server/scripts/Northrend/Ulduar/Ulduar/boss_mimiron.cpp
+++ src/server/scripts/Northrend/Ulduar/Ulduar/boss_mimiron.cpp
@@ -1411,11 +1411,24 @@ public:
                 {
                     float angle = (spinningUpOrientation * 2 * M_PI) / 100.0f;
                     me->SetFacingTo(angle);
+                    if (Unit* vehicle = me->GetVehicleBase())
+                    {
+                        vehicle->SetFacingTo(angle);
+                    }
 
                     spinningUpTimer = 0;
                 }
                 else
+                {
+                    float angle = (spinningUpOrientation * 2 * M_PI) / 100.0f;
+                    me->SetFacingTo(angle);
+                    if (Unit* vehicle = me->GetVehicleBase())
+                    {
+                        vehicle->SetFacingTo(angle);
+                    }
                     spinningUpTimer -= diff;
+
+                }
             }
 
             if (me->HasUnitState(UNIT_STATE_CASTING))
@@ -1508,13 +1521,14 @@ public:
                         {
                             vehicle->SetUInt32Value(UNIT_NPC_EMOTESTATE, EMOTE_STATE_CUSTOM_SPELL_01);
                             vehicle->HandleEmoteCommand(EMOTE_STATE_CUSTOM_SPELL_01);
-                            angle -= vehicle->GetOrientation();
+                            // angle -= vehicle->GetOrientation();
                             vehicle->SetFacingTo(angle);
                         }
 
                         spinningUpOrientation = (uint32)((angle * 100.0f) / (2 * M_PI));
                         spinningUpTimer = 1500;
                         me->SetFacingTo(angle);
+                        
                         me->CastSpell(p, SPELL_SPINNING_UP, true);
 
                         events.RescheduleEvent((Phase == 2 ? EVENT_SPELL_RAPID_BURST : EVENT_HAND_PULSE), 14500);
